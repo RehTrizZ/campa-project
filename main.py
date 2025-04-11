fe52bfbb924e2e19f7b10ef9e7d500f577ebcb76
alandi <alessandro@landiweb.it>
2025-04-11 15:54:52 +0200

    added photo and base

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 import cv2
+
+class Gradient:
+    def __init__(self, photo):
+        self.dict: dict = dict()
+        self.loadGradientBar(photo)
+    
+    def loadGradientBar(self, photo):
+        # Prende la barra in basso, trasforma ogni valore HEX di colore in una corrispondente temperature
+        # E la inserisci nel dizionario che sara composto da (HEX, valore in gradi)
+        pass
+
+class Pixel:
+    def __init__(self, value):
+        self.value = value
+
+    def getValue(self):
+        return self.value
+
+class Photo:
+    def __init__(self, filename):
+        self.img = ""
+        self.test(filename)
+
+    def test(self, filename: str):
+        self.img = cv2.imread("data/" + filename)
+        cv2.imshow("Photo: " + filename, self.img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    photo = Photo("IR_0014.jpg")
